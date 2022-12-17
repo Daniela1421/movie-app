b24640931040057c3f33688fee2929a246bf6c43
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-12-17 18:50:12 -0500

    feat: animated gradient structure and styles

--- App.tsx
+++ App.tsx
@@ -2,11 +2,24 @@ import 'react-native-gesture-handler';
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './src/navigation/Navigation';
+import { Fade } from './src/screens/Fade';
+import { GradientProvider } from './src/context/GradientContext';
+
+const AppState = ({children}: any) => {
+  return (
+    <GradientProvider>
+      {children}
+    </GradientProvider>
+  )
+}
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Navigation/>
+      <AppState>
+        <Navigation/>
+        {/* <Fade/> */}
+      </AppState>
     </NavigationContainer>
   )
 }
